bba06dd4011068e111333814790e2ccedc01f224
ShamRail <rail1999g102r@mail.ru>
2020-06-21 16:58:09 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -2,14 +2,40 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
+
+    private static final int POST_LIST_TOPIC = 1;
+
+    private static final int DATE_TIME = 5;
+
+    private static final int LINK = 0;
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.selectFirst(".forumTable").select(".altCol");
-        for (int i = 1; i < row.size(); i += 2) {
-            System.out.println(row.get(i).text());
+        for (int page = 1; page <= 5; page++) {
+            String url = String.format("https://www.sql.ru/forum/job-offers/%d", page);
+            parsePage(url);
         }
     }
+
+    private static void parsePage(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements tableRows = doc.select(".forumTable").first().getElementsByTag("tr");
+        for (Element row : tableRows) {
+            Element postListTopic = row.child(POST_LIST_TOPIC);
+            if (postListTopic.hasClass("postslisttopic")) {
+                Element date = row.child(DATE_TIME);
+                Element linkElement = postListTopic.child(LINK);
+                String link = linkElement.attr("href");
+                String text = linkElement.text();
+                System.out.println(String.format("%s %s %s", link, text, date.text()));
+            }
+
+        }
+    }
+
 }
